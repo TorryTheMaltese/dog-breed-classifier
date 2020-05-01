5e2ac55f96090f6250a46ac349d3c01c8daac491
TorryTheMaltese <torryiscute@gmail.com>
2020-05-02 01:53:42 +0900

    get image, save info

---
+++ app/src/main/java/com/example/dogbreedclassifier/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.dogbreedclassifier;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private int GET_GALLERY_IMAGE = 200;
+    public void onGalleryBtnClicked(View view){
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+        startActivityForResult(intent, GET_GALLERY_IMAGE);
+    }
+
+    private int GET_PHOTO_IMAGE = 1;
+    public void onPhotoBtnClicked(View view) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, GET_PHOTO_IMAGE);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == GET_PHOTO_IMAGE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            imageBitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+            byte[] byteArray = stream.toByteArray();
+            startNewActivity(byteArray);
+        }
+        else if (requestCode == GET_GALLERY_IMAGE && resultCode == RESULT_OK && data != null && data.getData() != null){
+            Log.e("TEST", data.getDataString());
+                startNewActivityByString(data.getDataString());
+        }
+    }
+
+    public void startNewActivity(byte[] byteArray){
+        Intent intent = new Intent(MainActivity.this, WriteInfoActivity.class);
+        intent.putExtra("imageByte", byteArray);
+        startActivity(intent);
+    }
+
+    public void startNewActivityByString(String imageString){
+        Intent intent = new Intent(MainActivity.this, WriteInfoActivity.class);
+        intent.putExtra("imageString", imageString);
+        startActivity(intent);
+    }
+}
