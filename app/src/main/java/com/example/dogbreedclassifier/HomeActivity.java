64cdc6a8432646f776cd8525f58785fdff9500e5
gingerK309 <dlatldydid18@naver.com>
2020-05-12 04:37:14 +0900

    insert database

--- app/src/main/java/com/example/dogbreedclassifier/HomeActivity.java
+++ app/src/main/java/com/example/dogbreedclassifier/HomeActivity.java
@@ -2,11 +2,14 @@ package com.example.dogbreedclassifier;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.hardware.Sensor;
@@ -18,13 +21,20 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Base64;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+
+
+import com.daimajia.swipe.SwipeLayout;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -40,6 +50,7 @@ import java.io.Serializable;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import static android.Manifest.permission.ACCESS_FINE_LOCATION;
@@ -64,16 +75,74 @@ public class HomeActivity extends AppCompatActivity implements SensorEventListen
     Sensor sensor;
     float sensorValue;
 
+    SwipeLayout swipe_card;
+    //------------추가------------
+    ImageButton imagebtn;
+    //--------------------------
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+//-------------------------추가--------------------------------------------
+
+        imagebtn = (ImageButton) findViewById(R.id.imagebtn);
+        imagebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+//--------------------------------------------------------------------------
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setNavigationBarColor(ContextCompat.getColor(this, R.color.colorPrimary));
+        }
         readFile(file);
+        externalFontFamily();
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         assert sensorManager != null;
         sensor = sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
 
-        ActivityCompat.requestPermissions(this, new String[] {ACCESS_FINE_LOCATION},1);
+        swipe_card = findViewById(R.id.sample1);
+        swipe_card.setShowMode(SwipeLayout.ShowMode.LayDown);
+        swipe_card.addDrag(SwipeLayout.DragEdge.Right, findViewById(R.id.bottom_wrapper));
+       //------------------추가----------------------
+        swipe_card.addDrag(SwipeLayout.DragEdge.Left, findViewById(R.id.swipe_left));
+        //-------------------------------------------
+        swipe_card.addSwipeListener(new SwipeLayout.SwipeListener() {
+
+            @Override
+            public void onStartOpen(SwipeLayout layout) {
+
+            }
+
+            @Override
+            public void onOpen(SwipeLayout layout) {
+
+            }
+
+            @Override
+            public void onStartClose(SwipeLayout layout) {
+
+            }
+
+            @Override
+            public void onClose(SwipeLayout layout) {
+
+            }
+
+            @Override
+            public void onUpdate(SwipeLayout layout, int leftOffset, int topOffset) {
+
+            }
+
+            @Override
+            public void onHandRelease(SwipeLayout layout, float xvel, float yvel) {
+
+            }
+        });
+
+        ActivityCompat.requestPermissions(this, new String[]{ACCESS_FINE_LOCATION}, 1);
 
         LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
@@ -123,16 +192,20 @@ public class HomeActivity extends AppCompatActivity implements SensorEventListen
         sensorManager.unregisterListener(this);
     }
 
-    public void getWeather(Location location){
+    public void externalFontFamily() {
+
+    }
+
+    public void getWeather(Location location) {
         double longitude = location.getLongitude();
         double latitude = location.getLatitude();
 
-        String url = "http://api.openweathermap.org/data/2.5/weather?lat="+latitude+"&lon="+longitude+"&appid="+api_key;
+        String url = "http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&appid=" + api_key;
         ReceiveWeatherTask receiveUseTask = new ReceiveWeatherTask();
         receiveUseTask.execute(url);
     }
 
-    private class ReceiveWeatherTask extends AsyncTask<String, Void, JSONObject>{
+    private class ReceiveWeatherTask extends AsyncTask<String, Void, JSONObject> {
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
@@ -140,24 +213,25 @@ public class HomeActivity extends AppCompatActivity implements SensorEventListen
 
         @Override
         protected JSONObject doInBackground(String... strings) {
-            try{
+            try {
                 HttpURLConnection conn = (HttpURLConnection) new URL(strings[0]).openConnection();
                 conn.setConnectTimeout(10000);
                 conn.setReadTimeout(10000);
                 conn.connect();
 
-                if(conn.getResponseCode()==HttpURLConnection.HTTP_OK){
+                if (conn.getResponseCode() == HttpURLConnection.HTTP_OK) {
                     InputStream is = conn.getInputStream();
                     InputStreamReader reader = new InputStreamReader(is);
                     BufferedReader in = new BufferedReader(reader);
 
                     String readed;
-                    while((readed = in.readLine()) != null){
+                    while ((readed = in.readLine()) != null) {
                         JSONObject jObject = new JSONObject(readed);
                         return jObject;
                     }
+                } else {
+                    return null;
                 }
-                else {return null;}
                 return null;
             } catch (MalformedURLException e) {
                 e.printStackTrace();
@@ -172,21 +246,22 @@ public class HomeActivity extends AppCompatActivity implements SensorEventListen
         @Override
         protected void onPostExecute(JSONObject result) {
             Log.e("API", result.toString());
-            if(result!=null){
+            if (result != null) {
                 String nowTemp = "";
                 double nowCTemp;
                 String main = "";
                 String description = "";
 
-                try{
+                try {
                     nowTemp = result.getJSONObject("main").getString("temp");
                     main = result.getJSONArray("weather").getJSONObject(0).getString("main");
                     description = result.getJSONArray("weather").getJSONObject(0).getString("description");
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
-                nowCTemp = Double.parseDouble(nowTemp)-273;
-                String msg = description+"\n"+"현재온도 : "+nowCTemp;
+                nowCTemp = Double.parseDouble(nowTemp) - 273;
+                String ctmp = String.format("%.1f", nowCTemp);
+                String msg = description + "\n" + "현재온도 : " + ctmp + "℃";
                 TextView text = findViewById(R.id.temp);
                 text.setText(msg);
             }
@@ -194,7 +269,7 @@ public class HomeActivity extends AppCompatActivity implements SensorEventListen
     }
 
     public void onSensorChanged(SensorEvent event) {
-        if(event.sensor==sensor) {
+        if (event.sensor == sensor) {
             sensorValue = event.values[0];
         }
     }
@@ -203,18 +278,22 @@ public class HomeActivity extends AppCompatActivity implements SensorEventListen
 
     }
 
-    public void setData(DogInfo dogInfo){
+    public void setData(DogInfo dogInfo) {
         ImageView savedImage = findViewById(R.id.saved_dog_image);
         savedImage.setImageURI(Uri.parse(dogInfo.imageUri));
 
         TextView savedName = findViewById(R.id.saved_dog_name);
+        TextView savedAge = findViewById(R.id.saved_dog_age);
+        TextView savedWeight = findViewById(R.id.saved_dog_weight);
         savedName.setText(dogInfo.name);
+        savedAge.setText(String.valueOf(dogInfo.age));
+        savedWeight.setText(String.valueOf(dogInfo.weight));
     }
 
-    private void readFile(File file){
-        int readCount=0;
-        if(file!=null && file.exists()){
-            try{
+    private void readFile(File file) {
+        int readCount = 0;
+        if (file != null && file.exists()) {
+            try {
                 FileInputStream fis = new FileInputStream(file);
                 readCount = (int) file.length();
                 byte[] buffer = new byte[readCount];
@@ -239,8 +318,10 @@ public class HomeActivity extends AppCompatActivity implements SensorEventListen
         }
     }
 
-    public void startNewActivity(View view){
+    public void startNewActivity(View view) {
         Intent intent = new Intent(HomeActivity.this, MainActivity.class);
         startActivity(intent);
     }
+
+
 }
--- app/src/main/java/com/example/dogbreedclassifier/WriteInfoActivity.java
+++ app/src/main/java/com/example/dogbreedclassifier/WriteInfoActivity.java
@@ -1,26 +1,38 @@
 package com.example.dogbreedclassifier;
 
 import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.BaseColumns;
 import android.provider.MediaStore;
 import android.util.Base64;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.NumberPicker;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
 
@@ -38,6 +50,10 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Objects;
+
 
 public class WriteInfoActivity extends AppCompatActivity {
 
@@ -49,19 +65,60 @@ public class WriteInfoActivity extends AppCompatActivity {
         String size;
         String fur;
     }
-
+//--------추가----------
+    myDBHelper myHelper;
+    EditText dog_name,dog_age,dog_weight;
+    SQLiteDatabase sqlDB;
+//-----------------
     String size;
     String fur;
     Uri profileImage;
+    byte[] imageByteArray = {};
 
     public static final String STRSAVEPATH = Environment.getExternalStorageDirectory()+"/testFolder/";
     public static final String SAVEFILENAME = "dogInfo.json";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write_info);
+        //--------------------- 추가---------------------------------------------
+        dog_name=(EditText)findViewById(R.id.dog_name);
+        dog_age=(EditText) findViewById(R.id.dog_age);
+        dog_weight=(EditText)findViewById(R.id.dog_weight);
+        ImageButton btn_insert=(ImageButton) findViewById(R.id.btn_insert);
+        myHelper = new myDBHelper(this);
+
+        btn_insert.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("WrongConstant")
+            @Override
+            public void onClick(View v) {
+              sqlDB = myHelper.getWritableDatabase();
+              sqlDB.execSQL("INSERT INTO dogTBL VALUES ('" +
+                      dog_name.getText().toString() + "',"
+                      + dog_age.getText().toString() +
+                      ", "+ dog_weight.getText().toString()+",'"+ size +"','"+fur+"');");
+                sqlDB.close();
+               Toast.makeText(getApplicationContext(),"추가완료",0).show();
+
+            }
+        });
+        //------------------------------------------------------------------
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setNavigationBarColor(ContextCompat.getColor(this, R.color.colorPrimary));
+        }
+        FrameLayout button = findViewById(R.id.result_btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                    getData();
+
+                }
+            }
+        });
 
         RadioGroup dogSize = findViewById(R.id.dog_size),
                 dogFur = findViewById(R.id.dog_fur);
@@ -84,7 +141,22 @@ public class WriteInfoActivity extends AppCompatActivity {
         getImage();
 
     }
-
+   //------------------------추가--------------------------------
+     public class myDBHelper extends SQLiteOpenHelper{
+     public myDBHelper(Context context){
+         super(context,"DOGINFO",null,1);
+     }
+     @Override
+       public void onCreate(SQLiteDatabase db){
+         db.execSQL("CREATE TABLE dogTBL (Dname CHAR(20) , Dage INTEGER, Dweight INTEGER, Dsize char(20), Dfur char(20) );");
+     }
+     @Override
+       public void onUpgrade(SQLiteDatabase db,int oldVersion,int newVersion){
+         db.execSQL("DROP TABLE IF EXISTS dogTBL");
+         onCreate(db);
+     }
+    }
+    //----------------------------------------------------------
     public void getImage() {
         ImageView image = findViewById(R.id.profileImg);
         if(getIntent().getExtras().getByteArray("imageByte") != null) {
@@ -106,8 +178,9 @@ public class WriteInfoActivity extends AppCompatActivity {
         return Uri.parse(path);
     }
 
+
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
-    public void getData(View view){
+    public void getData(){
         EditText dogName = findViewById(R.id.dog_name),
                 dogAge = findViewById(R.id.dog_age),
                 dogWeight = findViewById(R.id.dog_weight);
@@ -121,7 +194,7 @@ public class WriteInfoActivity extends AppCompatActivity {
         dog.imageUri = profileImage.toString();
 
         storeData(dog);
-        startNewActivity();
+        startNewActivity(profileImage);
     }
 
     public void storeData(DogInfo dog){
@@ -134,20 +207,20 @@ public class WriteInfoActivity extends AppCompatActivity {
 //        readFile(file);
     }
 
-    public String loadJSONFromAsset(){
-        String json = null;
-        try {
-            InputStream input = getAssets().open("DogInfo.json");
-            int size = input.available();
-            byte[] buffer = new byte[size];
-            input.read(buffer);
-            input.close();
-            json = new String(buffer, "UTF-8");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return json;
-    }
+//    public String loadJSONFromAsset(){
+//        String json = null;
+//        try {
+//            InputStream input = getAssets().open("DogInfo.json");
+//            int size = input.available();
+//            byte[] buffer = new byte[size];
+//            input.read(buffer);
+//            input.close();
+//            json = new String(buffer, "UTF-8");
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        return json;
+//    }
 
     private File makeDirectory(String dir_path){
         File dir = new File(dir_path);
@@ -200,8 +273,11 @@ public class WriteInfoActivity extends AppCompatActivity {
         return result;
     }
 
-    public void startNewActivity(){
+    public void startNewActivity(Uri imageUri){
         Intent intent = new Intent(WriteInfoActivity.this, ResultActivity.class);
+        intent.putExtra("imageUri", imageUri.toString());
         startActivity(intent);
     }
+
+
 }
